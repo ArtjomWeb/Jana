c33125c612e4e068dfc8bab7ae0170b96ab69f60
ArtjomWeb <148635889+ArtjomWeb@users.noreply.github.com>
2023-11-27 15:33:04 +0200

    reveal-effect

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+const revealSection = function(entries, observer){
+  const entry = entries[0];
+  console.log(entry)
+  if(!entry.isIntersecting) return
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target)
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.25
+})
+
+allSections.forEach(function(section) {
+  section.classList.add('section--hidden');
+  sectionObserver.observe(section);
+})
